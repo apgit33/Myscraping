5c4671bf6b47f02d415861b0b539017cbb654894
apgit33 <adrienpaturot@yahoo.fr>
2021-02-11 18:56:41 +0100

    Update route

--- public/index.php
+++ public/index.php
@@ -8,71 +8,33 @@ use App\Router\Router;
 
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 
-define('BASE_PATH', '/'); //adrienp/www/public/annonce/public
+define('BASE_PATH', '/cda/webscrapping'); //adrienp/www/public/annonce/public
 define('SERVER_URI', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . BASE_PATH);
 
-$router = new Router($_GET['url']);
+$router = new Router($_GET['url']?? "");
 $controllerView = new Controller();
 $controller = new UserController();
 $controllerE = new ExtractionController();
 
 session_start();
 
-$router->get('/', function () use ($controllerE) {
-    if (isset($_SESSION['user'])) {
-        $controllerE->render('index', [
-            'scraps' => isset($_SESSION['user']) ?? $controllerE->getAll()
-        ]);
-    } else {
-        header('Location: /signin');
-        exit();
-    }
-   
-});
-$router->get('/signin', function () use ($controllerView) {
-    $controllerView->render('form_signin');
-});
-$router->post('/signin', function ()use ($controller) {
-    $controller->checkFormLogin();
-});
+$router->get('/signin', "User#signin");
+$router->post('/signin', "User#checkFormLogin");
+
+$router->get('/signup', "User#signup");
+$router->post('/signup', "User#checkForm");
+
+$router->get('/user/logout', "User#logout");
+
+$router->get('/confirm-:slug', "User#validate");
+
+$router->get('/', "User#index");
 
 
-$router->get('/signup', function () use ($controllerView) {
-    $controllerView->render('form_signup');
-});
-$router->post('/signup', function ()use ($controller) {
-    $controller->checkForm();
-});
+$router->get('/user/account', "User#show");
+$router->post('/user/account', "User#checkFormAccount");
 
-
-
-
-
-$router->get('/confirm-:slug', function($slug) use ($controller){
-    $controller->validate($slug);
-    header("Refresh:5; url=".SERVER_URI."");
-});
-
-$router->get('/user/logout', function () use ($controller){
-    $controller->logout();
-});
-$router->get('/user/account', function () use ($controllerView) {
-    // session_start();
-    $controllerView->render('form_user',[
-        'user' => array_values((array)unserialize($_SESSION['user'])),
-        'SERVER_URI' => SERVER_URI
-    ]);
-});
-$router->post('/user/account', function () use ($controller) {
-    $controller->checkFormAccount();
-});
-
-// $router->get('/user/account/edit', function () {
-//     echo "compte edit user";
-// });
-// $router->post('/user/account/edit', function () {
-//     echo "POST compte edit user";
-// });
+//TODO
 $router->post('/user/account/delete', function () {
     echo "POST delete compte user";
 });
