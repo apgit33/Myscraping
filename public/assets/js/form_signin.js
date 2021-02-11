8421ee2d8087a503fa7f340241628fe20f0792ce
apgit33 <adrienpaturot@yahoo.fr>
2021-02-11 10:30:49 +0100

    Add & Update import/export function ajax

--- public/assets/js/form_signin.js
+++ public/assets/js/form_signin.js
@@ -1,36 +1,3 @@
-// *********** Début Form ***********
-//on récupère le formulaire
-const f_signup = document.getElementById('form_signin');
+import { form } from "./functions.js"
 
-//on récupère les div d'erreurs
-const f_erreur = document.getElementsByClassName('verif');
-
-//on ajoute un écouteur d'évènement sur la soumision du formulaire
-f_signup.addEventListener('submit', e => {
-    // on empêche l'envoie naturel du formulaire
-    e.preventDefault();
-
-    //on vide le champ des erreurs
-    for (let erreur of f_erreur) {
-        erreur.innerHTML ="";
-    }
-
-    //on récupère les données du formulaire sous forme de data (clé=>valeur)
-    const formData = new FormData(f_signup);
-
-    //on envoie les données du formulaire en ajax
-    fetch('signin', {
-        body: formData,
-        method: "POST"
-    })
-    .then(response => response.json())
-    .then(datas => {
-        if (datas.validation === true) {
-            location.href = "/"
-        }
-        for( var index in datas.erreurs) {
-            document.getElementById(index).innerHTML = datas.erreurs[index];
-        }
-    });
-});
-// *********** Fin Form ***********
+form('form_signin', 'signin')
